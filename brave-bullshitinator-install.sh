f0d84b90a0c1c05a0965672aed5372098a7dcda1
Mules Gaming <110064958+MulesGaming@users.noreply.github.com>
2024-08-19 13:17:28 -0500

    Added linux install script

---
+++ brave-bullshitinator-install.sh
@@ -0,0 +1,11 @@
+# Start
+echo "Starting Brave debullshitinator installer"
+echo "Brave debullshitinator is made by Mules Gaming. Find it here: https://github.com/MulesGaming/brave-debullshitinator"
+# Make dir
+sudo mkdir /etc/brave/policies/managed -p
+# Download file
+wget https://github.com/MulesGaming/brave-debullshitinator/releases/download/1.0.1/brave_debullshitinator-policies.json
+sudo cp brave_debullshitinator-policies.json /etc/brave/policies/managed
+rm brave_debullshitinator-policies.json
+# End
+echo "Brave debullshitinator installed"
